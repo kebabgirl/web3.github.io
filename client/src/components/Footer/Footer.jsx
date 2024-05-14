59a9e94f499532c958c4cac8e9c9f00f5d4dcee6
Olya Petruk <89353682+kebabgirl@users.noreply.github.com>
2024-05-14 16:19:38 +0300

    Add files via upload

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../../bootstrap.min.css";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer bg-dark">
+      <div>2024 PollUA ©</div>
+    </div>
+  );
+};
+
+export default Footer;
